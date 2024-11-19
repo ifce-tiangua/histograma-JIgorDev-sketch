6f8f12083761283cd45afcdd74fd4d081a9c24fc
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-19 09:58:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,75 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void exibir_histograma(char *hist[], int vendas[], int total_categorias)
-{
+void preenche_histograma(char *histograma[], int vendas, int categoria) {
+    int j;
+    for (j = 0; j < vendas; j++) {
+        histograma[categoria][j] = '*';
+    }
+}
+
+void imprimir_histograma(char *histograma[], int quantidade_de_vendas[], int numero_de_categorias) {
     int i, j;
+    for (i = 0; i < numero_de_categorias; i++) {
+        printf("%d ", quantidade_de_vendas[i]);
 
-    for (i = 0; i < total_categorias; i++)
-    {
-        if (vendas[i] == 0)
-        {
-            printf("0\n");
-        }
-        else
-        {
-            printf("%d ", vendas[i]);
-
-            for (j = 0; j < vendas[i]; j++)
-            {
-                printf("%c", hist[i][j]);
+        if (quantidade_de_vendas[i] == 0) {
+            printf("\n");
+        } else {
+            for (j = 0; j < quantidade_de_vendas[i]; j++) {
+                printf("%c", histograma[i][j]);
             }
-
             printf("\n");
         }
     }
 }
 
-int main()
-{
+int main() {
     int numero_de_categorias;
     scanf("%d", &numero_de_categorias);
 
-    if (numero_de_categorias == 0)
-    {
+    if (numero_de_categorias == 0) {
         printf("vazio\n");
         return 0;
     }
 
-    int vendas[numero_de_categorias];
-    char *histograma[numero_de_categorias];
-    int i, j;
+    int *quantidade_de_vendas = (int*) malloc(numero_de_categorias * sizeof(int));
+    char **histograma = (char**) malloc(numero_de_categorias * sizeof(char*));
 
-    for (i = 0; i < numero_de_categorias; i++)
-    {
-        scanf("%d", &vendas[i]);
+    int i;
+    for (i = 0; i < numero_de_categorias; i++) {
+        scanf("%d", &quantidade_de_vendas[i]);
 
-        if (vendas[i] == 0)
-        {
+        if (quantidade_de_vendas[i] == 0) {
             histograma[i] = NULL;
-        }
-        else
-        {
-            histograma[i] = (char *)malloc(vendas[i] * sizeof(char));
-
-            j = 0;
-            while (j < vendas[i])
-            {
-                histograma[i][j] = '*';
-                j++;
-            }
+        } else {
+            histograma[i] = (char*) malloc(quantidade_de_vendas[i] * sizeof(char));
+            preenche_histograma(histograma, quantidade_de_vendas[i], i);
         }
     }
 
-    exibir_histograma(histograma, vendas, numero_de_categorias);
+    imprimir_histograma(histograma, quantidade_de_vendas, numero_de_categorias);
 
-    for (i = 0; i < numero_de_categorias; i++)
-    {
-        if (histograma[i] != NULL)
-        {
+    for (i = 0; i < numero_de_categorias; i++) {
+        if (quantidade_de_vendas[i] > 0) {
             free(histograma[i]);
         }
     }
+    free(quantidade_de_vendas);
+    free(histograma);
 
     return 0;
 }
